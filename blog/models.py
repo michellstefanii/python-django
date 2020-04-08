00d09fd151b976f0c1d2cb3d9ed74d03e75d9a4a
Michel S. Stefani <michelstefani@empral.com.br>
2020-04-08 10:56:49 -0300

    Add model POST

---
+++ blog/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    STATUS  =   (
+        ('rascunho','Rascunho'),
+        ('publicado','Publicado'),
+    )
+    title  =   models.CharField(max_length=250)
+    slug    =   models.SlugField(max_length=250)#http://site.com/noticias/campeonato-braisleiro-2020/01/02/2020
+    author  =   models.ForeignKey(User,
+                                    on_delete=models.CASCADE)
+    content =   models.TextField()
+    published = models.DateTimeField(default=timezone.now)
+    created =   models.DateTimeField(auto_now_add=True)
+    changed =   models.DateTimeField(auto_now=True)
+    status  =   models.CharField(max_length=10,
+                                    choices=STATUS,
+                                    default='rascunhos')
+    class Meta:
+        ordering = ('-published',)
+
+    def __str__(self):
+        return self.title
+
+# Create your models here.
