4f131b0a40d5005cfed2e8d36e322fc12108c620
Michel S. Stefani <michelstefani@empral.com.br>
2020-04-08 16:38:07 -0300

    restructured

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager,self).get_queryset()\
+                                                .filter(status='publicado')
+
 class Post(models.Model):
     STATUS  =   (
         ('rascunho','Rascunho'),
@@ -18,6 +23,8 @@ class Post(models.Model):
     status  =   models.CharField(max_length=10,
                                     choices=STATUS,
                                     default='rascunhos')
+    objects = models.Manager()
+    publish = PublishedManager()
     class Meta:
         ordering = ('-published',)
 
